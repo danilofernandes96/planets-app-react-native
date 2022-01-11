f2cd96bbe57f9a1426f95fba52fe7060716d8f7d
Danilo Fernandes <danilo.fernandes@meliuz.com.br>
2022-01-11 16:45:28 -0400

    style: Add Container and ImageBackground styles

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import { ImageBackground } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-grow: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled(ImageBackground).attrs({
+  resizeMode: 'cover',
+  source: require('../../../assets/images/universe-background.png'),
+})`
+  flex-grow: 1;
+  justify-content: center;
+`;
