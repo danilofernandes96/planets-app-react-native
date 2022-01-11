f049776ae815e77306e9ef02ed646b3f3acb5d0e
Danilo Fernandes <danilo.fernandes@meliuz.com.br>
2022-01-11 16:46:30 -0400

    feat: Add Home component and include in App.tsx

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, Image} from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Image>
+      <Container />
+    </Image>
+  );
+};
+
+export default Home;
