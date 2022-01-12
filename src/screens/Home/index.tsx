f171221822f351b245fd29faad86c7d76d2f3818
Danilo Fernandes <danilo.fernandes@meliuz.com.br>
2022-01-12 16:29:54 -0400

    feat: Add api in Home component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet} from 'react-native';
 import Card from '../../components/Card';
+import {IPlanet} from '../../components/Card/types';
+import api from '../../services/api';
 import {Container, Image} from './styles';
 
 const Home: React.FC = () => {
+  const [planet, setPlanet] = useState<IPlanet[]>([]);
+
+  useEffect(() => {
+    api
+      .get('/planets')
+      .then(response => {
+        setPlanet(response.data);
+        console.log(response.data[0]);
+      })
+      .catch(e => console.log(e));
+  }, []);
+
   return (
-    <Image>
+    <Image source={require('../../../assets/images/universe-background.png')}>
       <Container>
-        <Card />
+        <FlatList
+          style={styles.flatlistContainer}
+          data={planet}
+          renderItem={({item}) => (
+            <Card
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              size={item.size}
+              temperature={item.temperature}
+            />
+          )}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+        />
       </Container>
     </Image>
   );
 };
+
+const styles = StyleSheet.create({
+  flatlistContainer: {
+    width: '100%',
+  },
+});
 
 export default Home;
