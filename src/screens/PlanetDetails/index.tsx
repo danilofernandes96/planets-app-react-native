4ee455f46fcccb00c3212e5ba1923fca2ff277e4
Danilo Fernandes <danilo.fernandes@meliuz.com.br>
2022-01-13 15:44:34 -0400

    feat: Add Details Screen

---
+++ src/screens/PlanetDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, Image} from './style';
+
+const PlanetDetails: React.FC = () => {
+  return (
+    <Image>
+      <Container>Hello</Container>
+    </Image>
+  );
+};
+
+export default PlanetDetails;
