d3047d52b3c314ba2c13117310621c5b4458ba57
Danilo Fernandes <danilo.fernandes@meliuz.com.br>
2022-01-19 10:22:29 -0400

    feat: Create routes file

--- App.tsx
+++ App.tsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from './src/screens/Home';
-import PlanetDetails from './src/screens/PlanetDetails';
-
-const Stack = createStackNavigator();
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'rgb(255, 255, 255)',
-    card: '#434071',
-    background: '#434071',
-    text: '#FFF',
-  },
-};
+import store from './src/store';
+import {Provider} from 'react-redux';
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer theme={MyTheme}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Details" component={PlanetDetails} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   );
 };
 
