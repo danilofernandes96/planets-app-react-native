221f8686265074e3b936106deb773437af6ac1f9
Danilo Fernandes <danilo.fernandes@meliuz.com.br>
2022-01-19 14:19:01 -0400

    fix: Fix warning react-native-gesture-handler

--- App.tsx
+++ App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import store from './src/store';
 import {Provider} from 'react-redux';
 import Routes from './routes';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 
 const App: React.FC = () => {
   return (
